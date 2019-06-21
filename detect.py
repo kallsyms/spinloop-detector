12825bd58b93c81c7669708af878fe6797d80ab9
Nick Gregory <ghost@capsule8.com>
2019-06-21 15:10:40 -0400

    properly look for overlapping BBs, fix typo preventing path constraining from working

--- detect.py
+++ detect.py
@@ -11,21 +11,34 @@ class SpinLoopExplorationTechnique(angr.ExplorationTechnique):
 
         self.after_first_loop_state = None
 
+    def _unnormalized_bb_addrs(self, bb_addr):
+        """
+        Get the list of all unnormalized BBs that contain the given BB addr.
+        """
+        bbs = set([bb_addr])
+
+        for n in self.cfg.nodes():
+            if n.addr <= bb_addr < n.addr + n.size:
+                bbs.add(n.addr)
+
+        return bbs
+
     def _is_at_loop_entry(self, addr):
         """
         The entry of the loop doesn't have to be the top of a IRSB (the preceeding block can jump
         into the middle of a IRSB that is at the end of a loop iteration)
         """
-        if addr in (successor.addr for successor in self.loop.entry.successors()):
+        if addr in self._unnormalized_bb_addrs(self.loop.entry.addr):
             return True
 
         return False
 
     def _loop_count(self, state):
         """
-        Returns the number of times the loop entry point has been hit, giving the index of the current loop iteration.
+        Returns the number of times the loop has iterated by counting the number of times the entry
+        address has been hit and subtracting 1.
         """
-        return len(list(filter(self._is_at_loop_entry, list(state.history.bbl_addrs) + [state.addr])))
+        return max(len(list(filter(self._is_at_loop_entry, list(state.history.bbl_addrs) + [state.addr]))) - 1, 0)
 
     def filter(self, simgr, state, **kwargs):
         print(hex(state.addr))
@@ -35,27 +48,26 @@ class SpinLoopExplorationTechnique(angr.ExplorationTechnique):
             return "deadended"
 
         # Continue to the beginning of the second iteration
-        if self._loop_count(state) < 2:
+        if self._loop_count(state) < 1:
             return "active"
 
         # constrain ourselves to take the same path as iter 1
-        # XXX: doesn't look like this is working?
-        if self._loop_count(state) < 3:
+        if self._loop_count(state) < 2:
             bbl_hist = list(state.history.bbl_addrs) + [state.addr]
-            for i, bb in enumerate(bbl_hist):
-                if any(bb == s.addr for s in self.loop.entry.successors()):
-                    first_loop_start = i
-                    break
+            first_loop_start = None
+            second_loop_start = None
 
             for i, bb in enumerate(bbl_hist):
-                if i <= first_loop_start:
-                    continue
-                if any(bb == s.addr for s in self.loop.entry.successors()):
-                    second_loop_start = i
-                    break
+                if self._is_at_loop_entry(bb):
+                    if first_loop_start is None:
+                        first_loop_start = i
+                    elif second_loop_start is None:
+                        second_loop_start = i
+                        break
 
             for bb1, bb2 in zip(bbl_hist[first_loop_start:second_loop_start], bbl_hist[second_loop_start:]):
-                if bb1 != bb1:
+                print(hex(bb1), hex(bb2))
+                if not self._unnormalized_bb_addrs(bb1).intersection(self._unnormalized_bb_addrs(bb2)):
                     print("Avoiding to constrain second loop to match path of first")
                     return "deadended"
 
@@ -64,15 +76,6 @@ class SpinLoopExplorationTechnique(angr.ExplorationTechnique):
         test_state = state.copy()
 
         for break_bb, _ in self.loop.break_edges:
-            bb_addrs = [break_bb.addr]
-
-            # BB addresses saved in history may not be the same as those in CFGs.
-            # Look for additional bb addresses that might point to this same bb
-            for pred in self.cfg.model.get_node(break_bb.addr).predecessors:
-                for target, kind in self.cfg.model.get_successors_and_jumpkind(pred):
-                    if target.addr == break_bb.addr and kind == "Ijk_Boring":
-                        bb_addrs.append(pred.addr)
-
             first_loop_constraint = None
             second_loop_constraint = None
 
@@ -80,7 +83,7 @@ class SpinLoopExplorationTechnique(angr.ExplorationTechnique):
                 if action.type != "constraint":
                     continue
 
-                if action.bbl_addr in bb_addrs:
+                if action.bbl_addr in self._unnormalized_bb_addrs(break_bb.addr):
                     if first_loop_constraint is None:
                         first_loop_constraint = action.constraint
                     elif second_loop_constraint is None:
@@ -89,7 +92,7 @@ class SpinLoopExplorationTechnique(angr.ExplorationTechnique):
 
             if first_loop_constraint is None:
                 print(f"No constraints around {break_bb.addr:#x}")
-                if self.loop.entry.addr in bb_addrs:
+                if self.loop.entry.addr in self._unnormalized_bb_addrs(break_bb.addr):
                     print("Loop entry not constrained by symbolic input")
                     return "deadended"
 
@@ -116,7 +119,8 @@ if __name__ == "__main__":
     p = angr.Project('./test.elf', auto_load_libs=False)
 
     main = p.loader.main_object.get_symbol('main')
-    cfg = p.analyses.CFGFast(normalize=True)
+    cfg = p.analyses.CFGFast()
+    cfg_norm = p.analyses.CFGFast(normalize=True)
     loops = p.analyses.LoopFinder()
 
     loop = loops.loops[0]
